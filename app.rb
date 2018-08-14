661140084125f7d42879bfbb01372d47141de0b1
rochellesagh <rsrochelli@gmail.com>
2018-08-14 20:24:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -9,9 +9,20 @@ class App < Sinatra::Base
 
   # This is a sample dynamic route.
   get "/hello/:name" do
+   puts params
+   
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
+  
+  #Create a new route for / goodbye/ :name that returns "Goodbye, (person's name)"
+   get "/goodbye/:name" do
+   puts params
+   
+    @user_name = params[:name]
+    "Goodbye #{@user_name}!"
+  end
+  
 
   # Code your final two routes here:
 
